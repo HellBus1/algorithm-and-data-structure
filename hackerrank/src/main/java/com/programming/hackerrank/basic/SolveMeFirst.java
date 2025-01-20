b8359d3234be701b73db1c57f72589eed53fc7d3
Syubban Fakhriya <syubbanfakhriya@gmail.com>
2025-01-21 01:07:35 +0700

    feat : add description annotation

--- hackerrank/src/main/java/com/programming/hackerrank/basic/SolveMeFirst.java
+++ hackerrank/src/main/java/com/programming/hackerrank/basic/SolveMeFirst.java
@@ -1,5 +1,8 @@
 package com.programming.hackerrank.basic;
 
+import org.springframework.context.annotation.Description;
+
+@Description("Problem link : https://www.hackerrank.com/challenges/solve-me-first/problem?isFullScreen=true")
 public class SolveMeFirst {
   int solveMeFirst(int a, int b) {
     return a + b;
